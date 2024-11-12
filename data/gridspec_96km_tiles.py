064e81f9c828362d2e40ca82fa9fe0df0b248801
Chris Owers <chris.owers@newcastle.edu.au>
2024-11-12 05:07:23 +0000

    started tiles list in seperate script. checked on few tiles in north, east and south of continent and so far seems outputs are reasonable

---
+++ data/gridspec_96km_tiles.py
@@ -0,0 +1,13 @@
+# below is a list of tiles that are to be included within the supratidal and coastal floodplain forest continential map
+# based on a gridspec of 96km (see notebooks on elevation and connectivity for more info)
+# TODO: Raf, please populate the list below, with a consistency from top to bottom of continent. Basically download the gridspec as a geojson (I assume this is quite possible) and select all appropriate tiles, then you be able to make the list below in top to bottom order (i.e. all the 60s first, starting in north tip of australia)
+# can you also do one line per gridspec row - just keeps it nice and neat
+# below is my 6 test tiles for now, not in order, just for testing
+tiles = [
+    (45, 58), # mary river
+    (42, 35), # nullarbor
+    (52, 31), # coorong
+    (59, 26), # corner inlet
+    (59, 26), # shoalhaven
+    (63, 31), # central coast
+]
